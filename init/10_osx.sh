10fb86051a061527365ec0b319e1e2747ad1cdcd
Adam Stankiewicz <sheerun@sher.pl>
2012-12-19 18:17:54 +0100

    feature: add osx initialization scripts

---
+++ init/10_osx.sh
@@ -0,0 +1,34 @@
+# OSX-only stuff. Abort if not OSX.
+[[ "$OSTYPE" =~ ^darwin ]] || return 1
+
+# Some tools look for XCode, even though they don't need it.
+# https://github.com/joyent/node/issues/3681
+# https://github.com/mxcl/homebrew/issues/10245
+if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
+  sudo xcode-select -switch /usr/bin
+fi
+
+# Install Homebrew.
+if [[ ! "$(type -P brew)" ]]; then
+  e_header "Installing Homebrew"
+  true | /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+fi
+
+if [[ "$(type -P brew)" ]]; then
+  e_header "Updating Homebrew"
+  brew update
+
+  # Install Homebrew recipes.
+  recipes=(git tree lesspipe htop-osx git-flow hub wget vim ssh-copy-id ctags)
+
+  list="$(to_install "${recipes[*]}" "$(brew list)")"
+  if [[ "$list" ]]; then
+    e_header "Installing Homebrew recipes: $list"
+    (brew list | grep ^$list$ > /dev/null) || brew install $list
+  fi
+
+  if [[ ! "$(type -P gcc-4.2)" ]]; then
+    e_header "Installing Homebrew dupe recipe: apple-gcc42"
+    brew install https://raw.github.com/Homebrew/homebrew-dupes/master/apple-gcc42.rb
+  fi
+fi

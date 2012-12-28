d6453bdc666729393b2edf5d9bdfcb2421fef44c
Adam Stankiewicz <sheerun@sher.pl>
2012-12-28 22:56:24 +0100

    fix: make st an alias and update LESSEDIT variable

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -2,7 +2,7 @@
 
 if [[ -z "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
   export EDITOR='st -w'
-  export LESSEDIT='st %f'
+  export LESSEDIT='st -w %f'
   alias e='st'
 else
   export EDITOR=$(type nano pico vi vim 2>/dev/null | sed 's/ .*$//;q')

ab59409d4dd9a957551b90deb88e6627301fe08b
Adam Stankiewicz <sheerun@sher.pl>
2012-12-29 20:51:28 +0100

    feature: new zstyles, moved to zstyle.sh

---
+++ source/50_zstyle.sh
@@ -0,0 +1,24 @@
+# make autocompletion faster by caching and prefix-only matching
+zstyle ':completion:*' accept-exact '*(N)'
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zsh/cache
+
+# fuzzy matching of completions for when you mistype them
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
+# get better autocompletion accuracy by typing longer words
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
+
+# ignore completion functions for commands you don't have
+zstyle ':completion:*:functions' ignored-patterns '_*'
+
+# highlight prefixes on autocompletion
+zstyle -e ':completion:*:default' list-colors 'reply=("${PREFIX:+=(#bi)($PREFIX:t)*==34=34}:${(s.:.)LS_COLORS}")';
+zstyle ':completion:*' squeeze-slashes true
+
+# completing process IDs with menu selection
+zstyle ':completion:*:*:kill:*' menu yes select
+zstyle ':completion:*:kill:*'   force-list always
+

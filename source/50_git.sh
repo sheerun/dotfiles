7c54d9fe25bd95bb0a4c3a51b703de52f65953a0
Adam Stankiewicz <sheerun@sher.pl>
2012-12-29 15:01:03 +0100

    feature: add shell git aliases for commonly used commands

--- source/50_git.sh
+++ source/50_git.sh
@@ -2,3 +2,13 @@
 
 # make hub an alias for git
 eval "$(hub alias -s)"
+
+# create shell aliases for most used git commands
+alias st='git st'
+alias ci='git ci'
+alias lg='git log'
+alias co='git co'
+alias add='git add'
+alias push='git push'
+alias pull='git pull'
+alias cached='git diff --cached'

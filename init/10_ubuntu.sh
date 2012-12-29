c916014c9d62b6e51895bcf68e68bd9f032eacbf
Adam Stankiewicz <sheerun@sher.pl>
2012-12-29 22:28:09 +0000

    feature: add ubuntu init script

---
+++ init/10_ubuntu.sh
@@ -0,0 +1,37 @@
+# Ubuntu-only stuff. Abort if not Ubuntu.
+[[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
+
+# Update APT.
+e_header "Updating APT"
+sudo apt-get -qq update
+
+# Install APT packages.
+packages=(
+  build-essential libssl-dev
+  git-core
+  tree
+  htop
+)
+
+list=()
+for package in "${packages[@]}"; do
+  if [[ ! "$(dpkg -l "$package" 2>/dev/null | grep "^ii  $package")" ]]; then
+    list=("${list[@]}" "$package")
+  fi
+done
+
+if (( ${#list[@]} > 0 )); then
+  e_header "Installing APT packages: ${list[*]}"
+  for package in "${list[@]}"; do
+    sudo apt-get -qq install "$package"
+  done
+fi
+
+# Install Git Extras
+if [[ ! "$(type -P git-extras)" ]]; then
+  e_header "Installing Git Extras"
+  (
+    cd ~/.dotfiles/libs/git-extras &&
+    sudo make install
+  )
+fi

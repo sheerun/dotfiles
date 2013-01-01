bae27fd4f52c301ccd3929e59e8252f9ef8a74c9
Adam Stankiewicz <sheerun@sher.pl>
2013-01-01 15:14:24 +0100

    feature: add ruby build script

---
+++ init/50_devel.sh
@@ -0,0 +1,31 @@
+# install ruby-build
+mkdir -p $HOME/.dotfiles/libs/rbenv/plugins
+cd $HOME/.dotfiles/libs/rbenv/plugins
+
+if [[ ! -h ./ruby-build ]]; then
+  ln -s ../../ruby-build;
+fi
+
+# install ruby
+if [[ "$(type -P rbenv)" ]]; then
+  versions=(1.9.3-p362)
+
+  list="$(to_install "${versions[*]}" "$(rbenv whence ruby)")"
+  if [[ "$list" ]]; then
+    e_header "Installing Ruby versions: $list"
+    for version in $list; do rbenv install "$version"; done
+    [[ "$(echo "$list" | grep -w "${versions[0]}")" ]] && rbenv global "${versions[0]}"
+    rbenv rehash
+  fi
+fi
+
+# install gems
+if [[ "$(type -P gem)" ]]; then
+  gems=(bundler awesome_print git-up)
+
+  list="$(to_install "${gems[*]}" "$(gem list | awk '{print $1}')")"
+  if [[ "$list" ]]; then
+    e_header "Installing Ruby gems: $list"
+    gem install $list
+  fi
+fi

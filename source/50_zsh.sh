ab05301881411edddb5bba7ca71a1c9be2041243
Adam Stankiewicz <sheerun@sher.pl>
2013-01-02 14:15:22 +0100

    feature: enable ls colors by default

--- source/50_zsh.sh
+++ source/50_zsh.sh
@@ -16,3 +16,10 @@ unsetopt correct_all
 
 # make rbenv init faster by disabling auto rehash
 eval "$(rbenv init - --no-rehash)"
+
+# Enable ls colors
+if [ "$DISABLE_LS_COLORS" != "true" ]
+then
+  # Find the option for using colors in ls, depending on the version: Linux or BSD
+  ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+fi

179ad7a4c61050187b100d9129ba11a5665437f6
Adam Stankiewicz <sheerun@sher.pl>
2013-01-24 01:47:22 +0100

    feature: always use vim for editing

--- zsh/50_editor.zsh
+++ zsh/50_editor.zsh
@@ -1,14 +1,3 @@
 # Editing
 
-if [[ -z "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
-  export EDITOR='subl -w'
-  export LESSEDIT='subl -w %f'
-  alias e='subl'
-else
-  export EDITOR=$(type nano pico vi vim 2>/dev/null | sed 's/ .*$//;q')
-  alias e="$EDITOR -w -z"
-fi
-
-export VISUAL="$EDITOR"
-
-alias e.='e .'
+export EDITOR="vim"

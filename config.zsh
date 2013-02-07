222626e1b39a15ffdceab20971cb3b6cc3f2cf0a
Adam Stankiewicz <sheerun@sher.pl>
2013-02-07 18:26:14 +0100

    Add ruby to install homebrew packages

--- config.zsh
+++ config.zsh
@@ -3,7 +3,7 @@ cdpath=( ~/Projects )
 
 # Recipes to install on OS X by homebrew.
 homebrew_recipes=(
-  coreutils findutils
+  coreutils findutils ruby
   git git-extras git-flow zsh htop-osx tree lesspipe wget
   vim ssh-copy-id ctags mobile-shell ack
   source-highlight

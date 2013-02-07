b9e609e3ea9ca9809a5ed4202b87cd2641d1568e
Adam Stankiewicz <sheerun@sher.pl>
2013-02-07 19:06:36 +0100

    fix: Remove ubuntu stuff that require sudo

--- init/50_ubuntu.zsh
+++ init/50_ubuntu.zsh
@@ -1,34 +1,6 @@
 # Ubuntu-only stuff. Abort if not Ubuntu.
 [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
 
-# Install APT packages.
-packages=(
-  build-essential libssl-dev
-  git-core
-  tree
-  htop
-  ctags
-  zsh
-)
-
-list=()
-for package in "${packages[@]}"; do
-  if [[ ! "$(dpkg -l "$package" 2>/dev/null | grep "^ii  $package")" ]]; then
-    list=("${list[@]}" "$package")
-  fi
-done
-
-if (( ${#list[@]} > 0 )); then
-  # Update APT.
-  e_header "Updating APT"
-  sudo apt-get -qq update
-
-  e_header "Installing APT packages: ${list[*]}"
-  for package in "${list[@]}"; do
-    sudo apt-get -qq install "$package"
-  done
-fi
-
 cat /etc/passwd | grep $USER | grep zsh >& /dev/null
 if [ $? -eq 0 ]; then
   sudo chsh -s $(which zsh) $USER

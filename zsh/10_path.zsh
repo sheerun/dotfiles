bc2d73f40762b2187cfafe8fbc4db7d2c824510e
Adam Stankiewicz <sheerun@sher.pl>
2013-02-15 02:32:04 +0100

    Add postgresql to path only on osx

--- zsh/10_path.zsh
+++ zsh/10_path.zsh
@@ -2,8 +2,8 @@
 export PATH=/usr/local/bin:$HOME/.python/bin/:$PATH
 export PATH=$PATH:$HOME/.dotfiles/libs/git-submodule-tools
 export PATH=$PATH:$HOME/.dotfiles/lib/git-extras/bin
-export PATHTH=$(brew --prefix postgresql)/bin:$PATH
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
+    export PATHTH=$(brew --prefix postgresql)/bin:$PATH
     export PATH="$(brew --prefix coreutils)/libexec/gnubin":$PATH
 fi

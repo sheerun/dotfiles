4c75c5b835b2b77c8ba65a7c202cd8168c6f86ba
Adam Stankiewicz <sheerun@sher.pl>
2013-03-05 13:20:03 +0100

    Automatically link ruby-build

--- init/60_devel.zsh
+++ init/60_devel.zsh
@@ -1,6 +1,9 @@
 # Make 1.9.3 default ruby installation.
 rbenv global 2.0
 
+# Install ruby-build
+mkdir ~/.rbenv/plugins && cd ~/.rbenv/plugins && ln -sf ~/.dotfiles/lib/ruby-build
+
 # Install most important ruby gems.
 if [[ -z $commands[gem] ]]; then
   gems=(bundler awesome_print git-up)

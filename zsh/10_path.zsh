fe3b054a830565050381c6c714f1aaa4a0661511
Adam Stankiewicz <sheerun@sher.pl>
2013-03-05 13:00:42 +0100

    Add cabal binaries to PATH

--- zsh/10_path.zsh
+++ zsh/10_path.zsh
@@ -2,6 +2,7 @@
 export PATH=/usr/local/bin:$HOME/.python/bin/:$PATH
 export PATH=$PATH:$HOME/.dotfiles/libs/git-submodule-tools
 export PATH=$PATH:$HOME/.dotfiles/lib/git-extras/bin
+export PATH=$PATH:$HOME/.cabal/bin
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
     export PATHTH=$(brew --prefix postgresql)/bin:$PATH

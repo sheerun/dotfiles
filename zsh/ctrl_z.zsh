82906375086045d88c44375828756779c49aafa2
Adam Stankiewicz <sheerun@sher.pl>
2013-03-20 14:15:31 +0100

    Fancy ctrl-z behavior

---
+++ zsh/ctrl_z.zsh
@@ -0,0 +1,11 @@
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    fg
+    zle redisplay
+  else
+    zle push-input
+    zle clear-screen
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey '^Z' fancy-ctrl-z

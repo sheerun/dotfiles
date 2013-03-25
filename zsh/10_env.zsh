666e40584c8ae4cd71b180c1c282bf8ac46363c1
Adam Stankiewicz <sheerun@sher.pl>
2013-03-25 23:40:46 +0100

    Dont repeat /usr/local/bin in PATH

--- zsh/10_env.zsh
+++ zsh/10_env.zsh
@@ -1,5 +1,5 @@
 # ensure /usr/local/bin has the highest precedence
-export PATH=$HOME/.bin:/usr/local/bin:$HOME/.python/bin/:$PATH
+export PATH=$HOME/.bin:$HOME/.python/bin/:$PATH
 export PATH=$PATH:$HOME/.dotfiles/libs/git-submodule-tools
 export PATH=$PATH:$HOME/.dotfiles/lib/git-extras/bin
 export PATH=$HOME/.cabal/bin:$PATH

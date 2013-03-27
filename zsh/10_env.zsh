7a0c418bd9c63502b0cea71c76d58e190df1bb30
Adam Stankiewicz <sheerun@sher.pl>
2013-03-27 13:37:44 +0100

    Add more characters as wordchars in zsh

--- zsh/10_env.zsh
+++ zsh/10_env.zsh
@@ -13,3 +13,8 @@ fi
 
 export EDITOR="vim"
 export LESSOPEN="| src-hilite-lesspipe.sh %s"
+
+# By default, zsh considers many characters part of a word (e.g., _ and -).
+# Narrow that down to allow easier skipping through words via M-f and M-b.
+export WORDCHARS='*?[]~&;!$%^<>'
+

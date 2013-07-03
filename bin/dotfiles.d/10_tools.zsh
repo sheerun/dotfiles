6bf08dd3314bec3d2f1532ecfcdeadc8c35cd31f
Adam Stankiewicz <sheerun@sher.pl>
2013-07-03 14:59:23 +0200

    Remove old dotfiles.d directory

--- bin/dotfiles.d/10_tools.zsh
+++
@@ -1,6 +0,0 @@
-# Given desired and installed items
-# return a list of uninstalled ones.
-function to_install() {
-  normalize() { for i in $1; do echo $i; done }
-  comm -23 <(normalize "$1" | sort) <(normalize "$2" | sort)
-}
--- bin/dotfiles.d/50_ubuntu.zsh
+++
@@ -1,7 +0,0 @@
-# Ubuntu-only stuff. Abort if not Ubuntu.
-[[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 0
-
-cat /etc/passwd | grep $USER | grep zsh >& /dev/null || {
-  echo "Changing your shell to zsh..."
-  sudo chsh -s $(which zsh)
-}
